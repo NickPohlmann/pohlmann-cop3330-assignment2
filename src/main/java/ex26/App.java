00eb4054f68ed0c76aee517991f7ffde8d3986ca
nicholaspohlmann <pohlmann.nick@gmail.com>
2021-06-09 13:03:42 -0400

    Almost done with ex 26

--- src/main/java/ex26/App.java
+++ src/main/java/ex26/App.java
@@ -4,5 +4,54 @@
  */
 package ex26;
 
+import java.util.Scanner;
 
+public class App {
+    private static final Scanner in = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        App myApp = new App();
+
+        String balanceStr = myApp.readInput("What is your balance? ");
+        String aprStr = myApp.readInput("What is the APR on the card (as a percent)? ");
+        String monthlyPaymentStr = myApp.readInput("What is the monthly payment you can make? ");
+        float balance = myApp.convertToFloat(balanceStr);
+        float aprPercentage = myApp.convertToFloat(aprStr);
+        float monthlyPayment = myApp.convertToFloat(monthlyPaymentStr);
+        float aprDecimal = myApp.convertPercentToDecimal(aprPercentage);
+        float dailyRate = myApp.computeDailyRate(aprDecimal);
+        PaymentCalculator calc = new PaymentCalculator();
+        int numMonths = calc.calculate(balance, dailyRate, monthlyPayment);
+        String outputString = myApp.generateOutputString(numMonths);
+        myApp.printOutputString(outputString);
+    }
+
+    private void printOutputString(String outputString) {
+        System.out.println(outputString);
+    }
+
+    private String generateOutputString(int numMonths) {
+        return String.format("It will take you %d months to pay off this card.",numMonths);
+    }
+
+    private float computeDailyRate(float aprDecimal) {
+        float dailyRate = aprDecimal / 365;
+        return dailyRate;
+    }
+
+    private float convertPercentToDecimal(float percentage) {
+        float decimal = percentage / 100;
+        return decimal;
+    }
+
+    private float convertToFloat(String str) {
+        float num = Float.parseFloat(str);
+        return num;
+    }
+
+    private String readInput(String str) {
+        System.out.print(str);
+        String input = in.nextLine();
+        return input;
+    }
+}
--- src/main/java/ex26/PaymentCalculator.java
+++ src/main/java/ex26/PaymentCalculator.java
@@ -3,7 +3,18 @@
  *  Copyright 2021 Nicholas Pohlmann
  */
 package ex26;
-import java.util.Scanner;
+
+import java.lang.Math;
 
 public class PaymentCalculator {
+    public int calculate(float balance, float dailyRate, float montlyPayment) {
+        float numMonthsUnrounded = 0;
+        numMonthsUnrounded = (float) ((-1/30) * Math.log(1 + (balance / montlyPayment) * (1-Math.pow(1+dailyRate, 30))) / Math.log(1+dailyRate));
+        if (numMonthsUnrounded % 1 != 0) {
+            int numMonth = (int) (numMonthsUnrounded + 1);
+            return numMonth;
+        }
+        int numMonth = (int) (numMonthsUnrounded);
+        return numMonth;
+    }
 }
