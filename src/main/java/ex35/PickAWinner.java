3aa34b383ef44c75b33f19f49c41fbd123876559
nicholaspohlmann <pohlmann.nick@gmail.com>
2021-06-11 11:47:47 -0400

    Started 35

---
+++ src/main/java/ex35/PickAWinner.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Nicholas Pohlmann
+ */
+
+package ex35;
+
+import java.util.Scanner;
+
+public class PickAWinner {
+    Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+}
