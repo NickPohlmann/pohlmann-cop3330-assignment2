39c4ac995970efe4fd7712ddebc7aeb5ab3fed7c
nicholaspohlmann <pohlmann.nick@gmail.com>
2021-06-11 12:07:46 -0400

    Finished ex 35

--- src/main/java/ex35/PickAWinner.java
+++ src/main/java/ex35/PickAWinner.java
@@ -5,12 +5,45 @@
 
 package ex35;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 public class PickAWinner {
     Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
+        PickAWinner newWinner = new PickAWinner();
 
+        ArrayList<String> contestantList = newWinner.getContestants();
+        String winner = newWinner.pickWinner(contestantList);
+        newWinner.printWinner(winner);
+    }
+
+    private void printWinner(String winner) {
+        System.out.println(String.format("The winner is... %s", winner));
+    }
+
+    private String pickWinner(ArrayList<String> contestantList) {
+        Random randNum = new Random();
+
+        int winningNum = randNum.nextInt(contestantList.size());
+        String winner = contestantList.get(winningNum);
+        return winner;
+    }
+
+    private ArrayList<String> getContestants() {
+        ArrayList<String> contestantList = new ArrayList<String>();
+        boolean enterMoreNames = true;
+        while (enterMoreNames) {
+            System.out.print("Enter a name: ");
+            String contestant = in.nextLine();
+            if (contestant.equals("")) {
+                enterMoreNames = false;
+            } else {
+                contestantList.add(contestant);
+            }
+        }
+        return contestantList;
     }
 }
